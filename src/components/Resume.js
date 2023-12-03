6647371d7ebbcbc4db211b6c9c55fc53efd2a6c5
Gal Parselani <Parselinho@gmail.com>
2023-12-03 22:56:17 +0200

    please

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,5 +1,6 @@
 import image from "../../public/images/aqua2.png";
 import treehouseSvg from "../../public/svg/treehouse.svg";
+import udemySvg from "../../public/svg/udemy-3.svg";
 
 class Resume {
   render() {
@@ -36,20 +37,38 @@ class Resume {
                 </li>
             </ul>
                 <div class="border"></div>
-            <h5>Tomer & Alon Marketing and Publicity, Ramat Gan - Data Analyst</h5>
+            <h5>Tomer & Alon Marketing and Publicity, Ramat Gan - System Manager and Team Leader</h5>
             <p>March 2019 - November 2022</p>
             <ul>
                 <li>
-                    Conducted in-depth data analysis to guide strategic decisions.
+                The company operates in the forex and bitcoin industry, specializing in connecting affiliates with brokers.
                 </li>
                 <li>
-                    Developed executive dashboards and reports, improving visibility into performance metrics.
+                My work involved handling operations across various countries, each with several affiliates and brands. My responsibilities included analyzing statistics and adjusting the system accordingly. For instance, if Brand 'A' performed exceptionally in the United Kingdom with Affiliate 'A' between 12:00-17:00, I would prioritize it in the system, followed by the arrangement of subsequent brands. This process was replicated for each country and affiliate based on specific regional data.
                 </li>
                 <li>
-                    Collaborated with teams to refine data collection and reporting practices.
+                Success Example: When I joined the company, there were only 0-2 deposits in Hungary per month. However, within four months, we achieved over 200 deposits during peak months, with an average of 50 deposits in subsequent months.
                 </li>
                 <li>
-                    Ensured data quality and accuracy in decision-making processes.
+                Another Example: Initially, when I suggested exploring brand opportunities in India, my manager was skeptical, citing a lack of prior deposits in the region and extensive industry experience. Despite this, I insisted on the potential in the Indian market. As a result, we identified a brand that consistently generated an average of 70 deposits per month
+                </li>
+                <li>
+                Additionally, I successfully managed an average of 13,000 leads per day single-handedly. Currently, this volume of work is distributed among three people, each handling 3-4K leads per day.
+                </li>
+                <li>
+                Conducted comprehensive data analysis, extracting actionable insights to guide strategic decision-making.
+                </li>
+                <li>
+                Utilized advanced statistical techniques to interpret complex datasets, enabling the identification of trends and patterns.
+                </li>
+                <li>
+                Developed and maintained dashboards and reports for executive teams, providing real-time visibility into key performance indicators.
+                </li>
+                <li>
+                Collaborated with cross-functional teams to define data requirements and ensure accurate and relevant reporting.
+                </li>
+                <li>
+                Implemented data quality measures, contributing to a culture of accuracy and reliability in data-driven decision-making.
                 </li>
             </ul>
           </section>
@@ -74,6 +93,17 @@ class Resume {
                 <a href='https://www.credential.net/1c3e2d8f-a879-46c6-a466-4b1eb0c9fc72#gs.0q0i5i' target='_blank' rel="noopener" title="navigate to the front end certification">certification for FEWD</a>
             </div>
             </div>
+            <div class="nopadding">
+            <div class='logoAndHeading'>
+            <img src="${udemySvg}" alt="udemy logo" loading="lazy">
+            <h4><a href="https://udemy.com" target='_blank' rel="noopener" title="visit udemy website">Udemy</a></h4>
+            </div>
+            <p>TechDegree in NodeJS and Express. 2023</p>
+            <div class="certDiv">
+                <a href='https://www.udemy.com/certificate/UC-d36709d8-6208-4206-bf7b-5be6b8086342/' target='_blank' rel="noopener" title="navigate to the full stack certification">certification for NodeJs and Express</a>
+            </div>
+            </div>
+
 
             <div class="border"></div>
 
@@ -81,6 +111,7 @@ class Resume {
             <h3>Skills</h3>
             </div>
             <ul>
+                <li><b>HTML, CSS, JavaScript, nodeJS, express, mongoDB, SQL, ReactJS</b></li>
                 <li>Proficient in Full-Stack Development, with a focus on JavaScript technologies.</li>
                 <li>Strong analytical and problem-solving capabilities.</li>
                 <li>Effective under pressure with a knack for efficient task execution.</li>
@@ -109,7 +140,7 @@ class Resume {
 
           <section class="resumeFinal">
             <h3>Seek Opportunities</h3>
-            <p>Eager to bring my technical expertise, innovative mindset, and leadership qualities to a challenging programming role. Open to exploring to collaborative and dynamic work environments, where I can contribute to and drive forward-thinking projects.</p>
+            <p>As an adept Full-Stack Developer, I am looking for opportunities to leverage my comprehensive knowledge in both front-end and back-end technologies. Skilled in HTML, CSS, JavaScript, alongside server-side language nodeJS, express. and database management like mongoDB and SQL, I aim to contribute to robust and scalable web applications. I'm eager to join a dynamic team that values innovation and offers growth opportunities in full-stack developmet</p>
           <section>
 
           </div>
